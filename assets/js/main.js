3cf79a5456db8868ecdc56f1cabe17a0b441e91f
Maria <mariacutipa.gonzales@gmail.com>
2017-05-18 17:41:23 -0500

    js vacio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,2 @@
-  $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-      $('.parallax').parallax();
-  });
+/*Iniciar*/
        
